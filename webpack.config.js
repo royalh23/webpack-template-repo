4bf7ef184f23b58d145015a37f232f4eab8f3163
Royal <royalhuseyn902@gmail.com>
2023-12-28 23:22:52 +0400

    Add HTMLWebpackPlugin in webpack.config.js and implement it

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,17 @@
-const path = require("path");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
-  devtool: "inline-source-map",
+  mode: 'development',
+  entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/template.html',
+    }),
+  ],
+  devtool: 'inline-source-map',
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
   },
 };
